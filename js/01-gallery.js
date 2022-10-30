b8406d23272450278a373fa710707fbf619108c8
Denis Boyko <deniskoms@gmail.com>
2022-10-30 13:49:29 +0200

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const divGallery = document.querySelector('.gallery');
+
+function creatGallery(galleryItems) {
+    return galleryItems
+        .map((item) => 
+           
+            `<div class="gallery__item">
+  <a class="gallery__link" href='${item.original}'>
+    <img
+      class="gallery__image"
+      src='${item.preview}'
+      data-source='${item.original}'
+      alt='${item.description}'
+    />
+  </a>
+</div>`).join('');
+    
+}
+
+const addGallery = creatGallery(galleryItems);
+divGallery.innerHTML = addGallery;
+divGallery.addEventListener('click', onImgClick);
+function onImgClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+      return
+    }
+    const instance = basicLightbox.create(`
+	<img src="${event.target.dataset.source}">
+`)
+    instance.show();
+    divGallery.addEventListener("keydown", (event) => {
+        if (event.code === 'Escape') {
+            instance.close()
+             document.removeEventListener("keydown", divGallery);
+        }
+    } )
+    
+} 
 console.log(galleryItems);
